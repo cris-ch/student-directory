459cc0596b27cf16a95ce3e141bf1b2dbc47eb06
foolfighter <cristian.christensen@gmail.com>
2021-11-27 11:54:20 +0100

    Improved code by using methods

--- directory.rb
+++ directory.rb
@@ -1,32 +1,93 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+@students = [] # an empty array accessible to all methods
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def input_students
+  puts "Please enter the informatiom of the students."
+  puts "To finish, just don't enter any name and hit return"
+  # get the first name
+  puts "Enter name:"
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # get the rest of the information for student
+    puts "Enter cohort:"
+    cohort = gets.chomp.gsub("\n","")
+    puts "Enter hobbies:"
+    hobbies = gets.chomp
+    puts "Enter country of birth:"
+    country = gets.chomp
+    # add the student hash to the array
+    @students << {name: name, cohort: cohort, hobbies: hobbies, country: country}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    puts "Enter name:"
+    name = gets.chomp
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_students_list
+  @students.each_with_index() do |student, idx|
+    puts "#{idx}. #{student[:name]}. Cohort: #{student[:cohort]}, Hobbies: #{student[:hobbies]}, Country: #{student[:country]} "
+  end
 end
 
-print_header
-print(students)
-print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_footer
+  if @students.length == 1 
+    puts "Overall, we have #{@students.count} great student".center(20)
+  else
+    puts "Overall, we have #{@students.count} great students".center(20)
+  end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:hobbies], student[:country]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+interactive_menu
